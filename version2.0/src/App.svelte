c4522eab3152289c801b1b77627ea94c6dfce99a
dev-seomoon <skm1104@daum.net>
2022-05-11 19:00:11 +0900

    Feat: 페이지 생성 & 라우팅

--- version2.0/src/App.svelte
+++ version2.0/src/App.svelte
@@ -1,30 +1,21 @@
 <script>
-	export let name;
+  import Router from 'svelte-spa-router';
+  import Home from './pages/Home.svelte';
+  import Whale from './pages/Whale.svelte';
+  import Todo from './pages/Todo.svelte';
+  import Setting from './pages/Setting.svelte';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Router routes={{
+    '/': Home,
+    '/whale': Whale,
+    '/todo': Todo,
+    '/setting': Setting
+  }} />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
---
+++ version2.0/src/pages/Home.svelte
@@ -0,0 +1,7 @@
+<header>
+  <nav>
+    <a href="/#/whale">웨일</a>
+    <a href="/#/todo">할일</a>
+    <a href="/#/setting">설정</a>
+  </nav>
+</header>
