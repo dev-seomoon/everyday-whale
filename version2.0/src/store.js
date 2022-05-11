f6fcad1590ddf76c9e2810fe65bae8a087f221d0
dev-seomoon <skm1104@daum.net>
2022-05-12 01:29:02 +0900

    Fix: todoList object localStorage 저장 시 stringrify & parsing

--- version2.0/src/store.js
+++ version2.0/src/store.js
@@ -3,11 +3,11 @@ import { writable } from 'svelte/store';
 export const name = writable(localStorage.getItem('name'));
 export const level = writable(localStorage.getItem('level'));
 export const exp = writable(localStorage.getItem('exp'));
-export const todoList = writable(localStorage.getItem('todoList'));
+export const todoList = writable(JSON.parse(localStorage.getItem('todoList')));
 export const doneCount = writable(localStorage.getItem('sumCount'));
 
 name.subscribe(value => localStorage.setItem('name', value));
 level.subscribe(value => localStorage.setItem('level', value));
 exp.subscribe(value => localStorage.setItem('exp', value));
-todoList.subscribe(value => localStorage.setItem('todoList', value));
+todoList.subscribe(value => localStorage.setItem('todoList', JSON.stringify(value)));
 doneCount.subscribe(value => localStorage.setItem('sumCount', value));
