96ae2b636f93670972d9585b4622b0b1d017970c
dev-seomoon <skm1104@daum.net>
2022-05-12 00:00:37 +0900

    Feat: Main페이지 whale 이미지, 유저 레벨, 이름 구현

--- version2.0/src/pages/Home.svelte
+++ version2.0/src/pages/Home.svelte
@@ -1,20 +1,45 @@
+<script>
+  import { name, level } from '../store.js';
+
+  const LEVEL_SCOPE = [0, 3, 8, 15, 26];
+
+  const getWhaleStatus = () => {
+    for (var i = 0; i < LEVEL_SCOPE.length; i++) {
+      if ($level < LEVEL_SCOPE[i]) break;
+    }
+    return i;
+  };
+
+  const status = getWhaleStatus();
+  
+  console.log($name, $level, status);
+
+</script>
+
 <main>
   <nav>
-    <a href="/#/whale"><img src="images/ic_whale.png" alt="웨일"/><span>웨일</span></a>
-    <a href="/#/todo"><img src="images/ic_todo.png" alt="할일" /><span>할일</span></a>
-    <a href="/#/setting"><img src="images/ic_setting.png" alt="설정" /><span>설정</span></a>
+    <a href="/#/whale"><img class="btn-img" src="images/ic_whale.png" alt="웨일"/><span>웨일</span></a>
+    <a href="/#/todo"><img class="btn-img" src="images/ic_todo.png" alt="할일" /><span>할일</span></a>
+    <a href="/#/setting"><img class="btn-img" src="images/ic_setting.png" alt="설정" /><span>설정</span></a>
   </nav>
+  <section id="main-whale">
+    <img class="whale-img swing" src="images/whale{status}.gif" alt="웨일 {status}단계" />
+    <h2>Lv.{$level} {$name}</h2>
+  </section>
 </main>
 
 <style>
+  /* container */
   main {
     height: 100%;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    align-items: center;
     background: white url("/images/background.png") no-repeat;
     overflow: visible;
   }
-  
+
+  /* menu */
   a {
     display: inline-flex;
     flex-direction: column;
@@ -22,7 +47,7 @@
     margin: 1.8rem;
   }
 
-  img {
+  .btn-img {
     width: 35px;
   }
 
@@ -31,5 +56,34 @@
     font-size: 1.68rem;
     font-weight: lighter;
     margin-top: 5px;
+  }
+
+  /* whale */
+  .whale-img {
+    width: 300px;
+  }
+
+  h2 {
+    font-size: 3rem;
+    color: #003c72;
+    text-shadow: 1px 1px 0 white, -1px -1px 0 white, 1px -1px 0 white, -1px 1px 0 white, 1px 1px 0 white;
+    text-align: center;
+    margin-top: 3rem;
+  }
+
+  .swing {
+    animation: swing 2s ease-in-out infinite;
+  }
+
+  @keyframes swing {
+    0% {
+      transform: translateY(0);
+    }
+    50% {
+      transform: translateY(15px);
+    }
+    100% {
+      transform: translateY(0);
+    }
   }
 </style>
