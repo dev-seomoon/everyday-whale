db94f66125ea370d0160b746c057df1911b7d703
dev-seomoon <skm1104@daum.net>
2022-05-12 22:50:16 +0900

    Refactor: BackButton 구조 변경

--- version2.0/src/components/BackButton.svelte
+++ version2.0/src/components/BackButton.svelte
@@ -1,10 +1,8 @@
-<main>
-  <nav>
-    <a href="/">
-      <img src="/images/ic_back.png" alt="뒤로가기" />
-    </a>
-  </nav>
-</main>
+<nav>
+  <a href="/">
+    <img src="/images/ic_back.png" alt="뒤로가기" />
+  </a>
+</nav>
 
 <style>
   img {
