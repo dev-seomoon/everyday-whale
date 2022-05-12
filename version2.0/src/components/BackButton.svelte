613929d034b8a634120665f5cd61ff0982b907c5
dev-seomoon <skm1104@daum.net>
2022-05-12 22:47:44 +0900

    Feat: 뒤로가기 버튼 컴포넌트

---
+++ version2.0/src/components/BackButton.svelte
@@ -0,0 +1,14 @@
+<main>
+  <nav>
+    <a href="/">
+      <img src="/images/ic_back.png" alt="뒤로가기" />
+    </a>
+  </nav>
+</main>
+
+<style>
+  img {
+    width: 30px;
+    margin: 15px;
+  }
+</style>
