54a1f683ee183abe5bfc66ac9668e4872f6e4ce8
dev-seomoon <skm1104@daum.net>
2022-05-13 01:18:19 +0900

    Feat: StoryButton 컴포넌트 작성

---
+++ version2.0/src/components/StoryButton.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let locked;
+  export let order;
+</script>
+
+<button type="button">
+  <img
+    src="/images/whale{order}_50px{locked ? '_grey' : ''}.png" alt="{order}번째 스토리 버튼"
+  />
+</button>
+
+<style>
+  button {
+    box-sizing: border-box;
+    border: 2px solid #006689;
+    border-radius: 8px;
+    width: 60px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
