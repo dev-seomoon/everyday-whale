5c64e6ec4dd6acb3f4893d2e094baf7239c84df9
dev-seomoon <skm1104@daum.net>
2022-05-17 01:08:27 +0900

    Feat: Todo Add 기능 구현

--- version2.0/src/pages/Home.svelte
+++ version2.0/src/pages/Home.svelte
@@ -1,8 +1,23 @@
 <script>
+  import { nanoid } from 'nanoid';
   import { todoList } from '../store.js';
   import Nav from '../components/Nav.svelte';
   import TodoItem from '../components/TodoItem.svelte';
   import MainWhale from '../components/MainWhale.svelte';
+
+  let todoValue = '';
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    todoList.set([...$todoList, {
+      id: nanoid(),
+      todo: todoValue,
+      isChecked: false,
+      addDate: new Date().toISOString().slice(0, 10),
+      finishDate: null,
+    }]);
+    todoValue = '';
+  };
 </script>
 
 <main>
@@ -14,9 +29,9 @@
         <TodoItem todo={todo} />
       {/each}
     </ul>
-    <form class="todo-box">
-      <input type="text" placeholder="새로운 할 일" />
-      <button type="button" />
+    <form class="todo-box" on:submit={addTodo} >
+      <input type="text" placeholder="새로운 할 일" bind:value={todoValue} />
+      <button type="button" on:click={addTodo} />
     </form>
   </section>
 </main>
